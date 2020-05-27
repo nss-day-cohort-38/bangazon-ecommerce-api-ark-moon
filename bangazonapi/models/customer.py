c46b16f556e6053064a6e15538e54c78d8460799
Jack Parsons <jcksnparsons@yahoo.com>
2020-05-27 14:37:34 -0500

    commit

--- bangazonapi/models/customer.py
+++ bangazonapi/models/customer.py
@@ -14,4 +14,4 @@ class Customer(models.Model):
         return f'{self.first_name} {self.last_name}'
 
     class Meta:
-        ordering = (F('address').asc(nulls_last=True),)
+        ordering = (F('id').asc(nulls_last=True),)
